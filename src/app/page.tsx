0420e185b7812dedcd921ded3434598795814114
DAVID AZULAY <davidazulay33@gmail.com>
2025-08-06 21:40:52 +0000

    כן

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -265,10 +265,10 @@ export default function Home() {
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="absolute top-1/2 -translate-y-1/2 left-2 bg-black/30 hover:bg-black/50 text-white z-10 h-10 w-10">
+          <CarouselPrevious className="absolute top-[35%] -translate-y-1/2 left-2 bg-black/30 hover:bg-black/50 text-white z-10 h-10 w-10">
             <ChevronLeft className="h-6 w-6" />
           </CarouselPrevious>
-          <CarouselNext className="absolute top-1/2 -translate-y-1/2 right-2 bg-black/30 hover:bg-black/50 text-white z-10 h-10 w-10">
+          <CarouselNext className="absolute top-[35%] -translate-y-1/2 right-2 bg-black/30 hover:bg-black/50 text-white z-10 h-10 w-10">
             <ChevronRight className="h-6 w-6" />
           </CarouselNext>
         </Carousel>
